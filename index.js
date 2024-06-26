fabdaf4f9d828d06b8552a251b13fac33cb5e3c3
JudahSan <baraka.judah@yahoo.com>
2024-06-26 23:12:15 +0300

    create earth

--- index.js
+++ index.js
@@ -1,63 +1,85 @@
 import * as THREE from "three";
-import { OrbitControls } from "jsm/controls/OrbitControls.js"
-
-// Create items needed for a scene: renderer, camera, scene
-
-// Renderer
-
-const w = window.innerWidth;
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import getStarField from "./src/getStarField.js";
+import { getFresnelMat } from "./src/getFresnelMat.js";
+console.log(`THREE REVISION: %c${THREE.REVISION}`, "color: #FFFF00");
+window.THREE = THREE;
 const h = window.innerHeight;
+const w = window.innerWidth;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, w / h, 0.1, 1000);
+camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-// set renderer height and width
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-// Camera
-
-const fov = 60;
-const aspect = w / h;
-const near = 0.4;
-const far = 10;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 4; // 2
-
-// Scene
-
-const scene = new THREE.Scene();
-
-// Object control
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.01;
-
-const geo = new THREE.IcosahedronGeometry(5.0, 2);
-const mat = new THREE.MeshStandardMaterial({
-  color: 0x00a1ff,
-  //   wireframe: true,
-  flatShading: true,
-}); // add material
-const mesh = new THREE.Mesh(geo, mat); // container for geometry and material
-scene.add(mesh);
-
-const wireMat = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  wireframe: true,
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
+scene.add(earthGroup);
+new OrbitControls(camera, renderer.domElement);
+const detail = 12;
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, detail);
+const material = new THREE.MeshStandardMaterial({
+  // color: 0xffff00,
+  // flatShading: true
+  map: loader.load("./textures/8k_earth_daymap.jpg"),
 });
-const wireMesh = new THREE.Mesh(geo, wireMat);
-wireMesh.scale.setScalar(1.001);
-mesh.add(wireMesh);
+const earthMesh = new THREE.Mesh(geometry, material);
+earthGroup.add(earthMesh);
+const lightsMat = new THREE.MeshBasicMaterial({
+  // color: 0x00ff00,
+  // transparent: true,
+  opacity: 0.8,
+  blending: THREE.AdditiveBlending,
+  map: loader.load("./textures/03_earthlights1k.jpg"),
+});
 
-// Add light
-const hemiLight = new THREE.HemisphereLight(0xff99ff, 0xaa5500);
-scene.add(hemiLight);
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+earthGroup.add(lightsMesh);
 
-const animate = (t = 0) => {
-  console.log(t);
+const cloudsMat = new THREE.MeshStandardMaterial({
+  map: loader.load("./textures/04_earthcloudmap.jpg"),
+  transparent: true,
+  opacity: 0.2,
+  blending: THREE.AdditiveBlending,
+  alphaMap: loader.load("./textures/05_earthcloudmaptrans.jpg"),
+});
+
+const cloudMesh = new THREE.Mesh(geometry, cloudsMat);
+cloudMesh.scale.setScalar(1.003);
+earthGroup.add(cloudMesh);
+
+const fresnelMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry, fresnelMat);
+glowMesh.scale.setScalar(1.01);
+earthGroup.add(glowMesh);
+// Add glowing light on the earth
+
+const stars = getStarField({ numStars: 1000 });
+scene.add(stars);
+
+// const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+// scene.add(hemiLight);
+const sunLight = new THREE.DirectionalLight(0xffffff);
+sunLight.position.set(-2, -0.5, 1.5);
+scene.add(sunLight);
+
+function animate() {
   requestAnimationFrame(animate);
-    // mesh.scale.setScalar(Math.cos(t * 0.001) + 1.0)
-    mesh.rotation.y = t * 0.0001;
+  // earthMesh.rotation.x += 0.001;
+  earthMesh.rotation.y += 0.002;
+  lightsMesh.rotation.y += 0.002;
+  cloudMesh.rotation.y += 0.00211;
+  glowMesh.rotation.y += 0.002;
   renderer.render(scene, camera);
-  controls.update();
-};
+}
 
 animate();
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", handleWindowResize, false);
